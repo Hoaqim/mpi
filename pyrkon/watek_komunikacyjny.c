81edbb219d53723ef7e9847988c42ee7bf37b9ef
Miłosz <101997323+SzramStaR@users.noreply.github.com>
2024-06-10 21:40:54 +0200

    watek_komunikacyjny basic

--- pyrkon/watek_komunikacyjny.c
+++ pyrkon/watek_komunikacyjny.c
@@ -9,18 +9,71 @@ void *startKomWatek(void *ptr)
     packet_t pakiet;
     /* Obrazuje pętlę odbierającą pakiety o różnych typach */
     while ( stan!=InFinish ) {
-	debug("czekam na recv");
+        packet_t pakiet;
+        MPI_Status status;
+	    debug("czekam na recv");
         MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        switch ( status.MPI_TAG ) {
-	    case FINISH: 
-                changeState(InFinish);
-	    break;
-	    case APP_PKT: 
-                debug("Dostałem pakiet od %d z danymi %d",pakiet.src, pakiet.data);
-	    break;
-	    default:
-	    break;
+        
+        //Przychodzi pakiet do wejścia na pyrkon, dostajemy broadcast
+        pthread_mutex_lock(&mutex_zegara);
+        if(zegar >= timestamp_pakietu){
+            zegar += 1 //wtedy dostajemy bilet i operacja zwiększa lamporta?
+        } else{
+            zegar = timestamp_pakietu + 1; 
         }
+        //aktualizacja zegara lamporta (max z własnego zegara i timestampu pakietu + 1)
+        pthread_mutex_unlock(&mutex_zegara)
+
+        //Obsługa żądań
+        //Tu by trzbea określić jakiś workshop, który jest teraz przetwarzany chyba
+        id_workshopu = workshop[rank][workshop_count[rank]] // <- aktualny workshop 
+        if(status.MPI_TAG == ACK){
+            //zakładając, że warsztaty idą od 1, a 0 to bilet na pyrkon
+            //i że mamy id workshopu w structcie pakietu
+            if(pakiet.id_workshopu == 0){
+                //Akcept na bilet na pyrkon
+            } else{
+                //Akcept na bilet na warsztat
+            }
+        }
+        //Jak zaakceptowany na aktualnie przetwarzany warsztat
+        if(status.MPI_TAG == ACK && pakiet.id_workshopu == id_workshopu){
+            zaakceptowani[rank] += 1; //chyba 
+        }
+        else if(status.MPI_TAG == REQUEST){
+            if(pakiet.id_workshopu == 0){
+                //Request na pyrkon
+            }
+            else if(id_workshopu == pakiet.id_workshopu){ // jak request na aktualny workshop
+                if(timestamp_pakietu < timestamp_requesta_warsztatu || timestamp_pakietu == timestamp_requesta_warsztatu && status.MPI_SOURCE < rank){
+                    //Jak ma albo mniejszy timestamp niż timestamp requesta, albo jak ma taki sam ale mniejszą range
+                    //Wysyłam akcepta na warsztat
+                }
+                else{
+                    //kolejka oczekujących na warsztat.append(MPI_SOURCE)
+                }
+            }
+            else{ //jak request na inny warsztat
+
+            }   
+        } else{ //status.MPI_TAG == FINISH
+            changeState(InFinish)
+            zakończono[rank] += 1
+            break;
+        }
+
+
+
+        // switch ( status.MPI_TAG ) {
+	    // case FINISH: 
+        //         changeState(InFinish);
+        //         zakończono[rank] += 1;
+	    // break;
+	    // case APP_PKT: 
+        //         debug("Dostałem pakiet od %d z danymi %d",pakiet.src, pakiet.data);
+	    // break;
+	    // default:
+	    // break;
+        // }
     }
 }
